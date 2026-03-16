6381914361c77b2634d9c98f95493108ab2ea191
Mister <mister_max@tutamail.com>
2026-03-16 17:51:50 +0200

    initial

---
+++ tg_weather.py
@@ -0,0 +1,67 @@
+from telethon import TelegramClient, events, functions, types, errors, Button
+from telethon.tl.types import Channel, Chat, User
+from telethon.tl.functions.users import GetFullUserRequest
+from dotenv import load_dotenv
+import subprocess, os, asyncio
+
+load_dotenv()
+
+APP_ID = os.getenv('app_id')
+APP_HASH = os.getenv('app_hash')
+TG_BOT_TOKEN = os.getenv('tg_bot_token')
+TG_BOT_SESSION = os.getenv('tg_bot_session')
+AUTHORIZED_USER_ID = os.getenv('authorized_user_id')
+WT_PATH = os.getenv('wt_path')
+
+client = TelegramClient(TG_BOT_SESSION, APP_ID, APP_HASH)
+
+SCRIPT_MAP = {
+    b"script1": WT_PATH + ' ir',
+    b"script2": WT_PATH + ' kv',
+    b"script3": WT_PATH + ' ir d',
+    b"script4": WT_PATH + ' kv d',
+    b"script5": WT_PATH + ' ir w',
+    b"script6": WT_PATH + ' kv w',
+}
+
+msg_ids = []
+
+@client.on(events.NewMessage(pattern='/start', from_users=int(AUTHORIZED_USER_ID)))
+async def start(event):
+    await event.respond(
+        "WEATHER FORECAST",
+        buttons=[
+            [Button.inline("HOME CURRENT", b"script1"),
+            Button.inline("WORK CURRENT", b"script2")],
+            [Button.inline("HOME DAILY", b"script3"),
+            Button.inline("WORK DAILY", b"script4")],
+            [Button.inline("HOME WEEKLY", b"script5"),
+            Button.inline("WORK WEEKLY", b"script6")],
+        ])
+
+@client.on(events.CallbackQuery)
+async def handler(event):
+    script_name = 'python ' + SCRIPT_MAP.get(event.data)
+    subprocess.run(script_name, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    with open('results', 'r') as f:
+        message = '\n'.join(f.read().split('\n')) 
+    message = await client.send_message(int(AUTHORIZED_USER_ID), message)
+    msg_ids.append(message.id)
+    msg_ids.append(message.id+1)
+    subprocess.run('rm results', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+@client.on(events.NewMessage(pattern='/clear', from_users=int(AUTHORIZED_USER_ID)))
+async def clear_handler(event):
+    current_id = event.message.id
+    chat = event.peer_id.user_id
+    await client.delete_messages(chat, msg_ids)
+    msg_ids.clear()
+
+async def main():
+    await client.start(bot_token=TG_BOT_TOKEN)
+    await client.run_until_disconnected()
+
+asyncio.run(main())
+
---
+++ wtbot.py
@@ -0,0 +1,65 @@
+import requests, sys, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ir = os.getenv('ir').split(' ')
+kv = os.getenv('kv').split(' ')
+
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {"timezone": "Europe/Kyiv"}
+
+if len(sys.argv) == 1:
+    print("select location !!!")
+    exit()
+
+if len(sys.argv) > 1:
+    params['current'] = ["temperature_2m", "relativehumidity_2m", "wind_speed_10m", "precipitation_probability"]
+    
+    if sys.argv[1] ==  'kv':
+        params['latitude'] = kv[0] 
+        params['longitude'] = kv[1]
+
+    elif sys.argv[1] ==  'ir':
+        params['latitude'] = ir[0] 
+        params['longitude'] = ir[1]
+
+if len(sys.argv) == 3:
+    if sys.argv[2] == 'd':
+        params['forecast_days'] = 1
+        params['hourly'] = ["temperature_2m", "precipitation_probability"]
+
+    elif sys.argv[2] == 'w':
+        params['forecast_days'] = 7
+        params['daily'] = ["temperature_2m_max", "temperature_2m_min", "precipitation_probability_max"]
+
+responses = requests.get(url, params=params)
+data = responses.json()
+
+if len(sys.argv) == 2:
+    message = "Temperature 👉  " + str(data['current']['temperature_2m']) + "°C\nRelative Humidity 👉  " + str(data['current']['relativehumidity_2m']) + "%\nPrecipitation Probability 👉  " + str(data['current']['precipitation_probability']) + "%\nWind Speed 👉  " + str(data['current']['wind_speed_10m']) + "km/h"
+    with open("results", 'w') as f: f.write(message)
+    print(message)
+
+elif len(sys.argv) == 3:
+    if sys.argv[2] == 'd':
+        for id, i in enumerate(data['hourly']['time']):
+            tmp = str(data['hourly']['temperature_2m'][id])
+            prc_prb = str(data['hourly']['precipitation_probability'][id])
+            message = i.split('T')[-1] + ' 👉  ' + tmp + '°C    ' + prc_prb + '%'
+            with open("results", 'a') as f: f.write(message+'\n')
+            print(message)
+    
+    elif sys.argv[2] == 'w':
+        for id, i in enumerate(data['daily']['time']):
+            max_tmp = str(data['daily']['temperature_2m_max'][id])
+            min_tmp = str(data['daily']['temperature_2m_min'][id])
+            max_prc_prb = str(data['daily']['precipitation_probability_max'][id]) 
+            message = i.split('-')[-1] + ' 👉  ' + max_tmp + '/' + min_tmp + '°C    ' + max_prc_prb + '%'
+            with open("results", 'a') as f: f.write(message+'\n')
+            print(message)
+
+
+
+
